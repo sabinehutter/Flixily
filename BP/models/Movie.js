d35d028f3cb44230b3d1fd31d96aac6a2c4073ac
Sabine-Hutter <Sabine.Hutter@ibm.com>
2021-01-06 21:44:32 -0500

    adding mongo models

--- BP/models/Movie.js
+++ BP/models/Movie.js
@@ -3,12 +3,57 @@ const Schema = mongoose.Schema;
 
 // CREATE THE MOVIE SCHEMA BASED ON SEEDDB.JS
 
+// const movieSchema = new Schema({
+// 	username: { type: String, required: true },
+// 	body: { type: String, required: true },
+// 	date: { type: Date, default: Date.now },
+// });
+
+// const Comment = mongoose.model("Comment", commentSchema);
+// const User = mongoose.model('Comment', UserSchema);
+
+//Movie Schema Added by Jennifer for below
 const movieSchema = new Schema({
-	username: { type: String, required: true },
-	body: { type: String, required: true },
-	date: { type: Date, default: Date.now },
+	id: {
+		type: Number,
+		unique: true,
+		required: true,
+		trim: true, 
+		autoIndex: true
+	  },
+	  userId: {
+		type: Number,
+		required: true,
+		trim: true
+	  },
+	movieId: {
+    type: Number,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  rating: {
+    type: Number,
+    required: false,
+    trim: true
+  },
+  image: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
 });
 
-const Comment = mongoose.model("Comment", commentSchema);
+const Movie = new mongoose.Schema(
+"Movie", movieSchema
+);
 
-module.exports = Comment;
+module.exports = Movie;
+
+
