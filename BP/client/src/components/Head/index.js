1bd07c5ab90f04182a67567821aac149f3651da6
Sabine-Hutter <Sabine.Hutter@ibm.com>
2021-01-06 22:04:41 -0500

    fixing errors

--- BP/client/src/components/Head/index.js
+++ BP/client/src/components/Head/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import useLogRender from "../../utils/useLogPath";
 
 function Nav() {
-	useLogRender();
+	// useLogRender();
 
 	return (
 		<nav>
-			<h1 className='mt-0 ml-0 mb-3 text-dark p-4 pt-3 pl-3'>Express Sequelize box</h1>
+			<h1 className='mt-0 ml-0 mb-3 text-dark p-4 pt-3 pl-3'>Flixily</h1>
 		</nav>
 	);
 }
